6b3e96a652d18b7292e88255780547d2321dfe22
James Pullar <james@pullar.me>
2015-11-03 23:45:43 -0800

    adding image model

---
+++ app/src/main/java/me/pullar/tigur/api/model/Image.java
@@ -0,0 +1,59 @@
+package me.pullar.tigur.api.model;
+
+/**
+ * Created by jamespullar on 11/3/15.
+ */
+public class Image {
+
+    private Integer id;
+    private String type;
+    private String title;
+    private String description;
+    private String link;
+    private boolean animated;
+    private Integer width;
+    private Integer height;
+    private Integer size;
+    private Integer views;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public boolean isAnimated() {
+        return animated;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+}
